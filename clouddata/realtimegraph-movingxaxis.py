0e1b29aab04b74ed36b468162160ac7d6e594979
Visnu Ganth <visnuganth07@gmail.com>
2020-10-13 13:31:12 +0530

    Added code to move x-axis dynamically in realtimegraph-movingxaxis. Done only for a single line.

--- clouddata/realtimegraph-movingxaxis.py
+++ clouddata/realtimegraph-movingxaxis.py
@@ -3,13 +3,11 @@ import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-plt.style.use('ggplot')
-
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('IMU_Data')
 
 mag_x, mag_y, mag_z, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, timestamp = ([] for i in range(10))
-fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, figsize=(10, 15))       #This creates the figure and 3 plots split in rows in which data is to be plotted
+fig, ax1 = plt.subplots(figsize=(10, 15))       #This creates the figure and plot in which data is to be plotted
 
 
 # Function to return the x,y and z values as a list of float values in order to plot them
@@ -31,16 +29,13 @@ def animate(i):
 
     global response
     global lastevalkey
+    global timestamp, mag_x
 
     data = (response.get('Items'))
 
     # Store all the necessary data for plotting in corresponding variables
-    device_id = data[0]['device_id']
-    timestamp.extend([item['timestamp'].split()[1] for item in data])  # Get only time value from the timestamp using split and "[1]"
-    accelerometer = [item['accelerometer'] for item in data]
-    split_axes(accelerometer, acc_x, acc_y, acc_z)
-    gyroscope = [item['gyroscope'] for item in data]
-    split_axes(gyroscope, gyro_x, gyro_y, gyro_z)
+    timestamp.extend([item['timestamp'].split()[1] for item in data])           # Get only time value from the timestamp using split and "[1]"
+
     magnetometer = [item['magnetometer'] for item in data]
     split_axes(magnetometer, mag_x, mag_y, mag_z)
 
@@ -55,31 +50,16 @@ def animate(i):
 
     lastevalkey = {k: lastitem.get(k) for k in lastitem.keys() if k in ['device_id', 'timestamp']}
 
-    # Clears the old lines that were plotted in the 3 plots
-    ax1.clear()
-    ax2.clear()
-    ax3.clear()
-
-    ax1.plot(timestamp, acc_x, label='X')
-    ax1.plot(timestamp, acc_y, label='Y')
-    ax1.plot(timestamp, acc_z, label='Z')
-    ax1.legend(frameon=False, loc='upper center', ncol=3)       #Places legend at the upper center in a single line without a border box
-    ax1.set_title('Accelerometer')
-
-    ax2.plot(timestamp, gyro_x, label='X')
-    ax2.plot(timestamp, gyro_y, label='Y')
-    ax2.plot(timestamp, gyro_z, label='Z')
-    ax2.legend(frameon=False, loc='upper center', ncol=3)
-    ax2.set_title('Gyroscope')
-
-    ax3.plot(timestamp, mag_x, label='X')
-    ax3.plot(timestamp, mag_y, label='Y')
-    ax3.plot(timestamp, mag_z, label='Z')
-    ax3.legend(frameon=False, loc='upper center', ncol=3)
-    ax3.set_title('Magnetometer')
-
+    ax1.clear()         #Clears the old line that was plotted
+    ax1.plot(timestamp, mag_x, label='X')       #Plot only mag_x data
+    ax1.legend(frameon=False, loc='upper center', ncol=1)       #Places legend at the upper center in a single line without a border box
     plt.xlabel('timestamp')
     plt.xticks(rotation=45)
+
+    #Uncomment for moving x-axis
+    # if len(timestamp) >= 20:
+    #     timestamp = timestamp[-len(timestamp) + 1:]
+    #     mag_x = mag_x[-len(mag_x) + 1:]
 
 
 # Get all the initial data from the table
@@ -94,9 +74,9 @@ except IndexError:
 
 lastevalkey = {k: lastitem.get(k) for k in lastitem.keys() if k in ['device_id', 'timestamp']}
 
-time.sleep(0.550)           # Data is being sent at x second intervals. After initial read, we have to wait x sec until next data is sent to DB
+time.sleep(0.550)                                 # Data is being sent at x sec intervals. After initial read, we have to wait x seconds until next data is sent to DB
 
-ani = animation.FuncAnimation(fig, animate, interval=500)       #Calls animate function at x sec interval repeatedly to plot the new points
+ani = animation.FuncAnimation(fig, animate, interval=500)      #Calls animate function at 2.2 sec interval repeatedly to plot the new points
 plt.show()                                                      #Show plot
 
 
